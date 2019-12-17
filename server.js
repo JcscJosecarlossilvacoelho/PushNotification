c00c028d4e3b81e75d2d542342104d57c81ff5ea
JcscJosecarlossilvacoelho <44404484+JcscJosecarlossilvacoelho@users.noreply.github.com>
2019-12-17 16:30:55 +0000

    Update server.js

--- server.js
+++ server.js
@@ -2,5 +2,4 @@ var express = require('express');
 var Push =  require('./src/Controller/PushNotification');
 const app = express();
 app.get("/Push",Push.index)
-app.get("/",  res.send("Hello"))
 app.listen(process.env.PORT || 3001)
