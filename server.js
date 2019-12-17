f993b12323227773587785f38100ddb696b194dd
JcscJosecarlossilvacoelho <44404484+JcscJosecarlossilvacoelho@users.noreply.github.com>
2019-12-17 16:23:30 +0000

    Update server.js

--- server.js
+++ server.js
@@ -2,4 +2,5 @@ var express = require('express');
 var Push =  require('./src/Controller/PushNotification');
 const app = express();
 app.get("/Push",Push.index)
+app.get("/",  res.send("Hello"))
 app.listen(3001)
