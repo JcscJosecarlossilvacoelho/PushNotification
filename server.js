c033bd45289c64b36c1057eac4b724c6d5e53e4a
JcscJosecarlossilvacoelho <44404484+JcscJosecarlossilvacoelho@users.noreply.github.com>
2019-12-18 14:42:03 +0000

    New

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var Push =  require('./src/Controller/PushNotification');
+const cors  = require('cors');
 const app = express();
+app.use(cors())
 app.get("/Push",Push.index)
 app.listen(process.env.PORT || 3001)
